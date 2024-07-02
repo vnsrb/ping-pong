d07cbfd9574249a9a6a74c838234fa3a8877915f
vnsrb <riosbarriosvenusacr@gmail.com>
2024-07-02 16:22:37 -0400

    Update pong.py
    pelota pintada en la ventana del juego

--- pong.py
+++ pong.py
@@ -6,6 +6,7 @@ init()
 
 # Constants
 SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
+pelota = 'pelota-de-tenis.png'
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BACK_COLOR = (173, 216, 230)
@@ -36,13 +37,27 @@ class Paddle(MainSprite):
     def __init__(self, sprite_img, x, y, width, height, speed):
         super().__init__(sprite_img, x, y, width, height, speed)
 
-    def update(self):
+    def update_l(self):
         keys = key.get_pressed()
 
-        if keys[K_UP] and self.rect.y > 0:
+        if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
 
+    def update_r(self):
+        keys = key.get_pressed()
 
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+
+class Pelota(MainSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y >= SCREEN_HEIGHT:
+            self.rect.y = 0
+            self.rect.x = randint(0, SCREEN_WIDTH - 50)
+
+
+pelota = Pelota(pelota, 320, 240, 50, 50, 5)
 finish = False
 run = True
 
@@ -54,6 +69,9 @@ while run:
 
     if not finish:
         screen.fill(BACK_COLOR)
+        pelota.reset()
+
+
 
     display.update()
 
